bc9f4a63203c3a2bdec71108670f2b0b66d3753b
Jon2G <jonyjouna@gmail.com>
2022-05-20 12:59:08 -0500

    Reporte

---
+++ Docs/code/vigenere.m
@@ -0,0 +1,27 @@
+        function normalizedCipher(obj)     
+           obj.Key=obj.getKeyVector(obj.Key); 
+           if(~obj.Encrypting)
+               %la llave de cifrado debe ser negativa para el descifrado
+               for i=1:size(obj.Key,2)
+                   obj.Key(i)=obj.Key(i)*-1;
+               end
+           end
+           
+           key_index=1;
+           key_size=size(obj.Key,2);
+           ASCII_LENGHT=size(obj.LanguageDefinition.Alphabet,2); % Tamaño del alfabeto reducido  
+           while(obj.Next()) %por cada caractér
+               if(obj.Letter<0)
+                   continue;
+               end
+               if(key_index>key_size)
+                   key_index=1;
+               end
+               key_value=obj.Key(key_index);
+               key_index=key_index+1;
+               obj.Letter=obj.Letter+key_value; %agregar el desplazamiento de la llave de cifrado
+               obj.Letter=mod(obj.Letter,ASCII_LENGHT); %modulo del valor de la letra y el tamaño del diccionario
+               obj.Letter=obj.LanguageDefinition.Alphabet(obj.Letter+1).Letter; %obtener la letra en la nueva posición del alfabeto cifrado base 1
+               obj.ResultText=append(obj.ResultText,obj.Letter); %agregar la letra al resultado del texto cifrado
+           end
+        end
